40dae163fed55c0079c98f77cb03a1e03586a1b7
chase-manning <chase@manning.dev>
2022-04-10 19:07:25 +0100

    :rocket: let there be life!!

--- src/state/lifeSlice.ts
+++ src/state/lifeSlice.ts
@@ -20,7 +20,36 @@ export const lifeSlice = createSlice({
   initialState,
   reducers: {
     step: (state) => {
-      state.cells = {};
+      let newCells = JSON.parse(JSON.stringify(state.cells));
+      for (let x = 0; x < state.width; x++) {
+        for (let y = 0; y < state.height; y++) {
+          let neighbors = 0;
+          for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+              if (i === 0 && j === 0) continue;
+              if (
+                state.cells[(x + i + state.width) % state.width] &&
+                state.cells[(x + i + state.width) % state.width][
+                  (y + j + state.height) % state.height
+                ]
+              ) {
+                neighbors++;
+              }
+            }
+          }
+          if (state.cells[x] && state.cells[x][y]) {
+            if (neighbors < 2 || neighbors > 3) {
+              newCells[x][y] = false;
+            }
+          } else {
+            if (neighbors === 3) {
+              if (newCells[x]) newCells[x][y] = true;
+              else newCells[x] = { [y]: true };
+            }
+          }
+        }
+      }
+      state.cells = newCells;
     },
     toggle: (state, action: PayloadAction<{ x: number; y: number }>) => {
       if (state.cells[action.payload.x]) {
@@ -37,10 +66,13 @@ export const lifeSlice = createSlice({
     pause: (state) => {
       state.playing = false;
     },
+    clear: (state) => {
+      state.cells = {};
+    },
   },
 });
 
-export const { step, toggle, play, pause } = lifeSlice.actions;
+export const { step, toggle, play, pause, clear } = lifeSlice.actions;
 
 export const selectCells = (state: RootState) => state.life.cells;
 export const selectWidth = (state: RootState) => state.life.width;

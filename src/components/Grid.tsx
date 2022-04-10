265b063598e3e35e96bbaba5d397a1a2473c9e4c
chase-manning <chase@manning.dev>
2022-04-10 18:24:37 +0100

    :sparkles: create grid

---
+++ src/components/Grid.tsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { selectHeight, selectWidth } from "../state/lifeSlice";
+import Cell from "./Cell";
+
+interface GridProps {
+  width: number;
+  height: number;
+}
+
+const StyledGrid = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(${(props: GridProps) => props.width}, 1fr);
+`;
+
+const Grid = () => {
+  const width = useSelector(selectWidth);
+  const height = useSelector(selectHeight);
+
+  return (
+    <StyledGrid width={width} height={height}>
+      {Array.from(Array(width * height).keys()).map((i) => (
+        <Cell index={i} />
+      ))}
+    </StyledGrid>
+  );
+};
+
+export default Grid;
